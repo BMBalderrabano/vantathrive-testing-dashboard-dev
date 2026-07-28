14b27f6f81c4ebf5f87b683b9b4ec5f521a2fe6f
Bruno <bruno@flywheel.so>
2026-07-28 13:55:51 -0300

    chore: added boolean true value to each event for triggering

--- src/app/api/talon/events/route.ts
+++ src/app/api/talon/events/route.ts
@@ -57,6 +57,7 @@ export async function POST(request: NextRequest) {
       body: JSON.stringify({
         profileId,
         type,
+        attributes: { [type]: true },
         responseContent: ['loyalty'],
       }),
     })
